81f0c06a50604c46493629de460a0ca298f8f541
chent08 <chentamir4@gmail.com>
2024-09-20 11:10:36 +0300

    BankAccount.java

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,7 +1,9 @@
 class BankAccount {
 	
-	private int balance = 0;
+    private int balance = 0;
     private int minBalance = -5000;
+    private String owner ;
+    
     
     public int getBalance() {
         return balance;
@@ -11,12 +13,40 @@ class BankAccount {
         return minBalance;
     }
     
+    public String getOwner() {
+    	return owner;
+    }
+    
+    public void setOwner(String owner){
+    	this.owner = owner;
+    }
+    
+    
+    
+    public void setBalance(int newBalance) {
+    	if (newBalance >= minBalance) {
+    		this.balance = newBalance;
+    		System.out.println("new balance: " + newBalance);
+    	}
+    	else {
+    		System.out.println("Cannot set balance below min balance of "+ minBalance);
+    	}
+    }
+    
+    
+   
  // Constructor to initialize balance and minBalance
+    public BankAccount(int balance, int minBalance, String owner) {
+        this.balance = balance;
+        this.minBalance = minBalance;
+        this.owner = owner;
+    }
+    
     public BankAccount(int balance, int minBalance) {
         this.balance = balance;
         this.minBalance = minBalance;
+  
     }
-    
    
     public boolean deposit(int amount){
     	if (amount>0) {
@@ -41,4 +71,8 @@ class BankAccount {
 	        }
 	        return false; // Transfer denied due to insufficient funds
 	    }
+	  
+	  public String toString() {
+		 return ("Balance of " + this.owner + " = " + this.balance);
+	  }
 }
