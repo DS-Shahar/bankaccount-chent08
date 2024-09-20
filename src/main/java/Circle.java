1ee328ade07de2af90c52b494e46c2f14a796601
Chen Tamir <chentamir4@gmail.com>
2024-09-20 21:23:36 +0300

    Circle.java
    Circle class

---
+++ src/main/java/Circle.java
@@ -0,0 +1,15 @@
+
+public class Circle {
+
+	private double radius;
+	
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+	
+	public double circleArea() {
+		return(this.radius * this.radius * Math.PI);
+	}
+	
+	
+}
